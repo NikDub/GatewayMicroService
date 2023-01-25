06e6553a4c08e78621184a30043e6aee236c67fe
Nikita Dubovski <nik.dubovski@gmail.com>
2023-01-25 11:22:04 +0300

    change id type from string to guid

--- AggregatorMicroService/Dto/Profiles/DoctorForCreateDto.cs
+++ AggregatorMicroService/Dto/Profiles/DoctorForCreateDto.cs
@@ -2,15 +2,15 @@
 
 public class DoctorForCreateDto
 {
-    public string AccountId { get; set; }
+    public Guid AccountId { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string MiddleName { get; set; }
     public DateTime DateOfBirth { get; set; }
-    public string StatusId { get; set; }
+    public Guid StatusId { get; set; }
     public int CareerStartYear { get; set; }
     public string AccountPhoneNumber { get; set; }
-    public string SpecializationId { get; set; }
+    public Guid SpecializationId { get; set; }
     public string SpecializationName { get; set; }
-    public string OfficeId { get; set; }
+    public Guid OfficeId { get; set; }
 }
--- AggregatorMicroService/Service/Abstraction/IHttpService.cs
+++ AggregatorMicroService/Service/Abstraction/IHttpService.cs
@@ -3,7 +3,7 @@
 public interface IHttpService
 {
     Task<IEnumerable<T>> HttpGetAsync<T>(string url, string attributeFromHeader);
-    Task<T> HttpGetByIdAsync<T>(string url, string id, string attributeFromHeader);
+    Task<T> HttpGetByIdAsync<T>(string url, Guid id, string attributeFromHeader);
     Task<T> HttpCreateAsync<T, TF>(string url, TF content, string attributeFromHeader);
-    Task HttpPutAsync<TF>(string url, string id, TF content, string attributeFromHeader);
+    Task HttpPutAsync<TF>(string url, Guid id, TF content, string attributeFromHeader);
 }
